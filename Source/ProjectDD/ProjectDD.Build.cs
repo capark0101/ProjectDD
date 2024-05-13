501374787eb8f5e66ee23c2d8937a2513b980ed3
capa5204 <capa5204@gmail.com>
2024-05-14 01:02:20 +0900

    [시스템] 프로젝트 초기 세팅

--- Source/ProjectDD/ProjectDD.Build.cs
+++ Source/ProjectDD/ProjectDD.Build.cs
@@ -7,13 +7,12 @@ public class ProjectDD : ModuleRules
 	public ProjectDD(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		
+		PrivateIncludePaths.Add("ProjectDD");
+        
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
-
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
 		
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
---
+++ Source/ProjectDD/Utils/DD_Enum.h
@@ -0,0 +1,10 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+//#include "DD_Enum.generated.h"
+
+/**
+ * 
+ */
